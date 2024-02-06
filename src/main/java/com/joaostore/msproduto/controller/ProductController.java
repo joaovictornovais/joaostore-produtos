9fc6cc15028fd561f04c499d57d3dd702a61144a
João Victor Novais <joao.dipaay@gmail.com>
2024-02-06 11:53:36 -0300

    cirando produto

--- src/main/java/com/joaostore/msproduto/controller/ProductController.java
+++ src/main/java/com/joaostore/msproduto/controller/ProductController.java
@@ -1,5 +1,27 @@
 package com.joaostore.msproduto.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.joaostore.msproduto.model.Product;
+import com.joaostore.msproduto.service.ProductService;
+
+@RestController
+@RequestMapping("/products")
 public class ProductController {
+	
+	private final ProductService productService;
+	
+	public ProductController(ProductService productService) {
+		this.productService = productService;
+	}
+	
+	@PostMapping
+	public ResponseEntity<Product> createProduct(@RequestBody Product product) {
+		return ResponseEntity.ok(productService.save(product));
+	}
 
 }
--- src/main/java/com/joaostore/msproduto/service/ProductService.java
+++ src/main/java/com/joaostore/msproduto/service/ProductService.java
@@ -1,5 +1,21 @@
 package com.joaostore.msproduto.service;
 
+import org.springframework.stereotype.Service;
+
+import com.joaostore.msproduto.model.Product;
+import com.joaostore.msproduto.repository.ProductRepository;
+
+@Service
 public class ProductService {
+	
+	private final ProductRepository productRepository;
+	
+	public ProductService(ProductRepository productRepository) {
+		this.productRepository = productRepository;
+	}
+	
+	public Product save(Product product) {
+		return productRepository.save(product);
+	}
 
 }
